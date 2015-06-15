79ad1cbf5240e26ffc9b49e2c832f3b58b8285ff
pataroka <predjov@yahoo.com>
2015-06-15 23:17:33 +0300

    p3 & 4

---
+++ 06.Other-Types-in-OOP-Homework/0304.GenericList/GenericListExec.cs
@@ -0,0 +1,32 @@
+﻿namespace _0304.GenericList
+{
+    using System;
+
+    public static class GenericListExec
+    {
+        public static void Main()
+        {
+            var list = new GenericList<int> { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            list.Version();
+
+            Console.WriteLine(list.Count);
+            list.Add(1);
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.Min());
+            Console.WriteLine(list.Max());
+            Console.WriteLine(list.Contains(11));
+            Console.WriteLine(list.FindIndex(3));
+            list.Insert(0, 5);
+            Console.WriteLine(list);
+            list.Remove(0);
+            Console.WriteLine(list);
+            list[0] = 18;
+            Console.WriteLine(list);
+            list.Clear();
+            Console.WriteLine(list);
+
+
+
+        }
+    }
+}
