27ca6379b4b809d5c82e3335c542e699dfc1f528
pataroka <predjov@yahoo.com>
2015-06-16 10:50:41 +0300

    p5

---
+++ 06.Other-Types-in-OOP-Homework/05.WordDocumentGenerator/WordDocumentGenerator.cs
@@ -0,0 +1,89 @@
+﻿namespace _05.WordDocumentGenerator
+{
+    using System.Drawing;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    using Novacode;
+
+    public static class WordDocumentGenerator
+    {
+        public static void Main()
+        {
+            DocX doc = DocX.Create(@"..\..\Result.docx");
+            
+            var boldFormat = new Formatting { FontFamily = new FontFamily("Calibri"), Size = 12D, Bold = true};
+            var boldUFormat = new Formatting { FontFamily = new FontFamily("Calibri"), Size = 12D, Bold = true, UnderlineColor = Color.Black};
+            var boldFFormat = new Formatting { FontFamily = new FontFamily("Calibri"), Size = 20D, Bold = true };
+
+            string temp = "role playing game";
+
+            using (StreamReader sr = new StreamReader(@"..\..\assets\text.txt"))
+            {
+                Paragraph title = doc.InsertParagraph().Append(sr.ReadLine().Trim()).FontSize(35).Font(new FontFamily("Calibri"));
+                title.Alignment = Alignment.center;
+                title.Bold();
+
+                Novacode.Image image = doc.AddImage(@"..\..\assets\rpg-game.png");
+                Picture picture = image.CreatePicture();
+                picture.Height = 231;
+                picture.Width = 600; 
+
+                Paragraph p1 = doc.InsertParagraph();
+
+                p1.AppendPicture(picture);
+
+                p1.AppendLine(sr.ReadLine()).FontSize(12).Font(new FontFamily("Calibri"));
+                p1.AppendLine(sr.ReadLine()).FontSize(12).Font(new FontFamily("Calibri"));
+                p1.AppendLine(sr.ReadLine()).FontSize(12).Font(new FontFamily("Calibri"));
+                p1.ReplaceText(temp, temp, false, RegexOptions.None, boldFormat);
+                temp = "grand prize";
+                p1.ReplaceText(temp, temp, false, RegexOptions.None, boldUFormat);
+
+                var list = doc.AddList(listType: ListItemType.Bulleted);
+
+                doc.AddListItem(list, sr.ReadLine());
+                doc.AddListItem(list, sr.ReadLine());
+                doc.AddListItem(list, sr.ReadLine());
+                doc.InsertList(list);
+
+                Paragraph p2 = doc.InsertParagraph();
+                p2.AppendLine(sr.ReadLine()).FontSize(12).Font(new FontFamily("Calibri"));
+
+                Table table = doc.AddTable(4, 3);
+                table.Design = TableDesign.LightListAccent1;
+                table.Alignment = Alignment.center;
+                string[] arr = Regex.Split(sr.ReadLine(), @"\s+");
+                table.Rows[0].Cells[0].Paragraphs[0].Append(arr[0]).FontSize(12).Font(new FontFamily("Calibri"));
+                table.Rows[0].Cells[1].Paragraphs[0].Append(arr[1]).FontSize(12).Font(new FontFamily("Calibri"));
+                table.Rows[0].Cells[2].Paragraphs[0].Append(arr[2]).FontSize(12).Font(new FontFamily("Calibri"));
+                for (int i = 1; i < 4; i++)
+                {
+                    arr = Regex.Split(sr.ReadLine(), @"\s+");
+                    for (int j = 0; j < 3; j++)
+                    {
+                        table.Rows[i].Cells[j].Paragraphs[0].Append(arr[j]).FontSize(12).Font(new FontFamily("Calibri"));
+                    }
+                }
+             
+                doc.InsertTable(table);
+
+                Paragraph p3 = doc.InsertParagraph();
+                p3.AppendLine(sr.ReadLine()).FontSize(12).Font(new FontFamily("Calibri"));
+
+                Paragraph footer1 = doc.InsertParagraph().Append(sr.ReadLine().Trim()).FontSize(20).Font(new FontFamily("Calibri"));
+                footer1.Alignment = Alignment.center;
+                temp = "SPECTACULAR";
+                footer1.ReplaceText(temp, temp, false, RegexOptions.None, boldFFormat);
+
+                Paragraph footer2 = doc.InsertParagraph().Append(sr.ReadLine().Trim()).FontSize(35).Font(new FontFamily("Calibri"));
+                footer2.Alignment = Alignment.center;
+                footer2.Bold();
+                footer2.Color(Color.CornflowerBlue);
+                footer2.UnderlineColor(Color.CornflowerBlue);
+            }
+
+            doc.SaveAs(@"..\..\Result.docx");
+        }   
+    }
+}
